b743a9b44fafc563397429395bec6bcf7dcce673
p1nh0 <p1nh0.c0d1ng@gmail.com>
2018-04-22 18:33:54 +0100

    implemented sequencer modes
    up, down, up&down, palindrome, random step, random step & pitch

--- Devi_firmware/Devi_firmware/Devi_firmware/Sketch.cpp
+++ Devi_firmware/Devi_firmware/Devi_firmware/Sketch.cpp
@@ -17,6 +17,7 @@ Mozzi mode = HIFI
 #include <Ead.h>
 #include <Metronome.h>
 #include <mozzi_midi.h>
+#include <mozzi_rand.h>
 
 #include "GLOBALS.h"
 
@@ -36,13 +37,14 @@ void knobSetup();
 void setRGB(byte r, byte g, byte b);
 void allLedsOff();
 
-void readControls();
 void readButtons();
 void readKnobs();
 void setMode();
 void execMode(); 
 void execMode0(); void execMode1(); void execMode2(); void execMode3(); 
 void execMode4(); void execMode5(); void execMode6(); void execMode7();
+
+void incStep();
 //End of Auto generated function prototypes by Atmel Studio
 
 #define CONTROL_RATE 512
@@ -57,11 +59,13 @@ unsigned int att=25, dcy=450;
 uint8_t pitch[6] = {36, 39, 43, 46, 48, 29};
 
 boolean mute[6] = {true, true, true, true, true, true};
-uint8_t step = 0, steps = 6;
+int8_t step = 0, steps = 6;
 uint16_t bpm = 4*120, _bpm = bpm; 
 boolean seqTrigger = 1; // actually runs at twice the audible sequencer rate to turn bpm led indicator on/off (1-triggers envelopes and led on; 0- triggers led off)
 Metronome metro(60000/bpm/2); // 120bpm
 boolean seqSwitch = true;
+uint8_t stepMode = 0; // up, down, up&down, palindrome, random step, random step & pitch
+boolean stepDir = 1; // 1-up, 0-down
 
 uint8_t filter_mode; // 0-LP, 1-BAND, 1-HP
 uint8_t fx_mode; // 0-OFF, 1-<<, 2->>, 3-&, 4-|, 5-^
@@ -77,6 +81,7 @@ void setup(){
 	buttonSetup();
 	knobSetup();
 	//Serial.begin(9600);
+	randSeed();
 	startMozzi(CONTROL_RATE);
 	setupFastAnalogRead(FASTEST_ADC);
 
@@ -84,7 +89,8 @@ void setup(){
 }
 
 void updateControl(){
-	readControls();
+	readButtons();
+	readKnobs();
 	setMode();
 	execMode();
 }
@@ -100,79 +106,7 @@ void loop(){
   audioHook();
 }
 
-void setMode(){
-	if (button[7].changed && !button[7].state && !modeLock) // trigger on button released
-	{ 
-		allLedsOff();
-		exec_mode++;
-		if(exec_mode>7) exec_mode = 0;
-	
-		switch(exec_mode){ // 1-IntSeq, 2-RndSeq, 3-Stack, 4-Arp, 5-Play, 6-Rnd, 7-Sys
-			case 0: // external sequencer (red)
-				setRGB(HIGH, LOW, LOW);
-				break;
-			case 1: // internal sequencer (orange)
-				setRGB(HIGH, HIGH, LOW);
-				if(seqTrigger) metro.start();
-				break;
-			case 2: // randomize sequence (purple)
-				setRGB(HIGH, LOW, HIGH);
-				break;
-			case 3: // external play stack notes (green)
-				setRGB(LOW, HIGH, LOW);
-				break;
-			case 4: // external play arpeggio (cian)
-				setRGB(LOW, HIGH, HIGH);
-				break;
-			case 5: // internal play (blue)
-				setRGB(LOW, LOW, HIGH);
-				break;
-			case 6: // randomize synth (white)
-				setRGB(HIGH, HIGH, HIGH);
-				break;
-			case 7: // system (off)
-				setRGB(LOW, LOW, LOW);
-				break;
-		
-			default:
-			break;
-		}
-	}
-}
-
-void execMode(){
-	switch(exec_mode){ // 1-IntSeq, 2-RndSeq, 3-Stack, 4-Arp, 5-Play, 6-Rnd, 7-Sys
-		case 0: // external sequencer (red)
-			metro.stop();
-			break;
-		case 1: // internal sequencer (orange)
-			execMode1();
-		break;
-		
-		case 2: // randomize sequence (purple)
-			metro.stop();
-			break;
-		case 3: // external play stack notes (green)
-			metro.stop();
-			break;
-		case 4: // external play arpeggio (cian)
-			metro.stop();
-			break;
-		case 5: // internal play (blue)
-			metro.stop();
-			break;
-		case 6: // randomize synth (white)
-			metro.stop();
-			break;
-		case 7: // system (off)
-			metro.stop();
-			break;
-		
-		default:
-			break;
-	}
-}
-// FUNCTIONS
+// DEVI FUNCTIONS
 void buttonSetup(){
 	button[0].init(PINBUT1);
 	button[1].init(PINBUT2);
@@ -219,10 +153,6 @@ void setRGB(byte r, byte g, byte b){
 	digitalWrite(PINBLUE, b);
 }
 
-void readControls(){
-	readButtons();
-	readKnobs();
-}
 void readButtons(){
 	button[0].read();
 	button[1].read();
@@ -245,28 +175,101 @@ void readKnobs(){
 	knob[5].read();
 }
 
+void setMode(){
+	if (button[7].changed && !button[7].state && !modeLock) // trigger on button released
+	{
+		allLedsOff();
+		exec_mode++;
+		if(exec_mode>7) exec_mode = 0;
+		
+		switch(exec_mode){ // 1-IntSeq, 2-RndSeq, 3-Stack, 4-Arp, 5-Play, 6-Rnd, 7-Sys
+			case 0: // external sequencer (red)
+			setRGB(HIGH, LOW, LOW);
+			break;
+			case 1: // internal sequencer (orange)
+			setRGB(HIGH, HIGH, LOW);
+			if(seqTrigger) metro.start();
+			break;
+			case 2: // randomize sequence (purple)
+			setRGB(HIGH, LOW, HIGH);
+			break;
+			case 3: // external play stack notes (green)
+			setRGB(LOW, HIGH, LOW);
+			break;
+			case 4: // external play arpeggio (cian)
+			setRGB(LOW, HIGH, HIGH);
+			break;
+			case 5: // internal play (blue)
+			setRGB(LOW, LOW, HIGH);
+			break;
+			case 6: // randomize synth (white)
+			setRGB(HIGH, HIGH, HIGH);
+			break;
+			case 7: // system (off)
+			setRGB(LOW, LOW, LOW);
+			break;
+			
+			default:
+			break;
+		}
+	}
+}
+void execMode(){
+	switch(exec_mode){ // 1-IntSeq, 2-RndSeq, 3-Stack, 4-Arp, 5-Play, 6-Rnd, 7-Sys
+		case 0: // external sequencer (red)
+		metro.stop();
+		break;
+		case 1: // internal sequencer (orange)
+		execMode1();
+		break;
+		
+		case 2: // randomize sequence (purple)
+		metro.stop();
+		break;
+		case 3: // external play stack notes (green)
+		metro.stop();
+		break;
+		case 4: // external play arpeggio (cian)
+		metro.stop();
+		break;
+		case 5: // internal play (blue)
+		metro.stop();
+		break;
+		case 6: // randomize synth (white)
+		metro.stop();
+		break;
+		case 7: // system (off)
+		metro.stop();
+		break;
+		
+		default:
+		break;
+	}
+}
+
 void execMode1(){
 	// set mutes and step length 
-	for(int i = 0; i < 6; i++){
-		if (button[i].changed)
-		{
-			if (button[6].state)
+	if(!button[7].state){
+		for(int i = 0; i < 6; i++){
+			if (button[i].changed)
 			{
-				steps = i+1; 
-				digitalWrite(PINLED2, i>1);
-				digitalWrite(PINLED3, i>2);
-				digitalWrite(PINLED4, i>3);
-				digitalWrite(PINLED5, i>4);
-				digitalWrite(PINLED6, i>5);
-				selectLock = true;
-			}
-			else 
-			{
-				if(button[i].state) mute[i] = !mute[i];
+				if (button[6].state)
+				{
+					steps = i+1;
+					digitalWrite(PINLED2, i>1);
+					digitalWrite(PINLED3, i>2);
+					digitalWrite(PINLED4, i>3);
+					digitalWrite(PINLED5, i>4);
+					digitalWrite(PINLED6, i>5);
+					selectLock = true;
+				}
+				else
+				{
+					if(button[i].state) mute[i] = !mute[i];
+				}
 			}
 		}
 	}
-	
 	// set pitch
 	if( knob[0].changed ) pitch[0] = (knob[0].val >> 1) + 24;
 	if( knob[1].changed ) pitch[1] = (knob[1].val >> 1) + 24;
@@ -284,7 +287,7 @@ void execMode1(){
 	else{
 		if( knob[5].changed ) pitch[5] = (knob[5].val >> 1) + 24;
 	}
-		
+	
 	// set sequencer start/stop
 	if( button[6].changed && !selectLock && !button[6].state) // trigger on button released
 	{
@@ -301,8 +304,21 @@ void execMode1(){
 		}
 	}
 	
-	// release SELECT lock
-	if(!button[6].state) selectLock = false; 
+	// set step mode
+	if (button[7].state)
+	{
+		//modeLock = true;
+		for (int i = 0; i < 6; i++){
+			if (button[i].changed) {
+				stepMode = i;
+				modeLock = true; 
+			} 
+		}
+	}
+	else modeLock = false; 
+	
+	// release select lock
+	if(!button[6].state) selectLock = false;
 		
 	// sequencer update
 	if ( metro.ready() ){
@@ -318,8 +334,7 @@ void execMode1(){
 			digitalWrite(PINLED7, HIGH);
 			carrier1.setFreq( mtof(pitch[step]) );
 			if ( mute[step] )   dca.start(att, dcy);
-			step++;
-			if ( step > steps-1 )   step = 0;
+			if ( steps > 1 ) incStep();
 		}
 		else{ // update leds running a 2x the metronome speed
 			digitalWrite(PINLED7, LOW);
@@ -328,4 +343,58 @@ void execMode1(){
 	}
 	
 	gain = (int) dca.next();
+}
+
+void incStep(){
+	switch(stepMode){
+		case 0: // up
+			step++;
+			if ( step > steps-1)   step = 0;
+			break;
+		case 1: // down 
+			step--;
+			if ( step < 0)   step = steps-1;
+			break;
+		case 2: // up&down
+			if(stepDir){ // up
+				 step++;
+				 if ( step > steps-1){
+					 step = steps-2;
+					 stepDir = 0;
+				 }
+			}
+			else { // down
+				step--;
+				if ( step < 0){
+					step = 1;
+					stepDir = 1;
+				}
+			}
+			break;
+		case 3: // palindrome 
+			if(stepDir){ // up
+				step++;
+				if ( step > steps-1){
+					step = steps-1;
+					stepDir = 0;
+				}
+			}
+			else { // down
+				step--;
+				if ( step < 0){
+					step = 0;
+					stepDir = 1;
+				}
+			}
+			break;
+		case 4: // random step
+			step = rand(steps);
+			break;
+		case 5: // random step & pitch
+			step = rand(steps);
+			pitch[step] = rand(knob[step].val >> 1) + 24;
+			break;
+		default:
+		break;
+	}
 }
