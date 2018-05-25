1073e3b2bdea46e8f8a53711ef1f2e1b17f63c3f
p1nh0 <p1nh0.c0d1ng@gmail.com>
2018-05-25 19:23:31 +0100

    working version with minor ui control bugs
    still implementing mode and function locks

--- firmware/Devi-Lite/Devi-Lite/Devi-Lite/Globals.h
+++ firmware/Devi-Lite/Devi-Lite/Devi-Lite/Globals.h
@@ -8,23 +8,6 @@
 #ifndef GLOBALS_H_
 #define GLOBALS_H_
 
-//! Control update rate in Hertz (must be a power of 2).
-#define CONTROL_RATE 512
-
-//! Compiler optimization flag
-#define OPTIMIZATION "O3"
-
-//! Total number of voices. This value will be often used as there are not only 6 voices, but 6 leds, 6 potentiometers, etc.
-#define NUMVOICES 6
-
-//! Total number of Modes
-#define NUMMODES 4
-
-//! Rotary switch lower split: number of pins connected to PORTC 
-#define ROTLOWER 8
-//! Rotary switch upper split: number of pins connected to PORTD
-#define ROTUPPER 4
-
 #define bit_get(p,m) ((p) & (m))
 #define bit_set(p,m) ((p) |= (m))
 #define bit_clear(p,m) ((p) &= ~(m))
@@ -38,6 +21,34 @@
 #endif
 
 
+//! Control update rate in Hertz (must be a power of 2).
+#define CONTROL_RATE 512
+
+//! Compiler optimization flag
+#define OPTIMIZATION "O3"
+
+//! Total number of Modes
+#define NUMMODES 4
+
+//! Rotary switch lower split: number of pins connected to PORTC 
+#define ROTLOWER 8
+//! Rotary switch upper split: number of pins connected to PORTD
+#define ROTUPPER 4
+
+//! Total number of voices. This value will be often used as there are not only 6 voices, but 6 leds, 6 potentiometers, etc.
+#define NUMVOICES 6
+
+//! Minimum pitch 
+#define MINPITCH 24
+
+//! Minimum BPM
+#define MINBPM 40
+
+//! Minimum filter frequency
+#define MINFILTER 40
+//! Maximum filter frequency
+#define MAXFILTER 12000
+
 /*! \defgroup UI
 * User interface variables
 *  @{
@@ -47,7 +58,7 @@
 * 
 * Button 1 is on bit0, button 6 is on bit5, Function button is on bit6 and Mode button is on bit7.
 */
-uint8_t buttons;
+uint8_t buttons = B11111111;
 /*! Previous buttons' values for state change detection*/ 
 uint8_t _buttons; 
 
@@ -57,14 +68,24 @@ uint8_t rotary;
 //! Previous RotarySwitch value for state change detection
 uint8_t _rotary; 
 
-//! Knobs array with values from 0 to 127
+//! Knobs array with values from 0 to 255
 uint8_t knobs[NUMVOICES];
 
 //! Previous knob values for state change detection
-uint8_t knobs[NUMVOICES];
+uint8_t _knobs[NUMVOICES];
 
 //! Mode variable set by mode button 
 uint8_t mode;
+
+//! Mode button locked?
+boolean modeLock = false;
+//! Previous Mode lock
+boolean _modeLock = true;
+//! Function button locked ? 
+boolean functionLock = false; 
+//! Previous Function lock
+boolean _functionLock = true;
+
 /*! \brief Mode led (rgb) colors
 *
 * I made a mistake in the schematic: the green and blue pins are switched.
@@ -74,6 +95,52 @@ uint8_t modeColors[NUMMODES] = {B00100000, B00010000, B00111000, B00001000};
 							  //green,     blue,      white,     red
  
 /*! @} */ // end of UI variables 
+
+/*! \defgroup Sequencer
+*
+* Sequncer Mode variables.
+*  @{
+*/
+
+//! \brief Step mutes
+//boolean stepMute[6] = {false, false, false, false, false, false};
+uint8_t stepMutes = B00000000; 
+//! \brief Current sequencer step
+int8_t step = 0;
+//! \brief Selected number of steps
+uint8_t steps = 6; // selected number of steps
+//! \brief Beat subdivisions (x1, x2, x4, x8, x32, x64)
+uint8_t subdivisions[8] {1, 2, 4, 8, 16, 32, 64, 128};
+//! \brief Selected subdivision
+uint8_t subdivision = 2; 
+//! \brief Beats per minute
+uint16_t bpm = 120;
+//! \brief Previous beats per minute
+uint16_t _bpm = bpm;
+/*! \brief Sequencer toggle (On/Off) 
+*
+* Note that the sequencer can be kept running while throughout all Modes. 
+*/
+boolean seqOnOff = true; 
+/*! \brief Sequencer clock signal 
+*
+* The sequencer actually runs at twice the audible sequencer rate to turn bpm led indicator on/off (1-triggers envelopes and led on; 0- triggers led off)
+*/
+boolean seqTrigger = 1; 
+//! Metronome
+Metronome metro(60000/bpm/2); // calculate bpm
+//! Step mode ( up, down, up&down, palindrome, random step, random step & random pitch amount)
+uint8_t stepMode = 0;
+//! Sequencer direction
+boolean stepDir = 1; // 1-up, 0-down
+/*! Led sequence
+*
+* The sequnce position is indicated by the led that is turned off.
+*\n Note that step mutes also leave leds turned off. 
+*/
+//uint8_t ledSequence[6] = {B01111010, B01111001, B01110011, B01101011, B01011011, B00111011};
+uint8_t ledSequence[6] = {0, 1, 3, 4, 5, 6};
+/*! @} */
 
 
 #endif /* GLOBALS_H_ */
--- firmware/Devi-Lite/Devi-Lite/Devi-Lite/Sketch.cpp
+++ firmware/Devi-Lite/Devi-Lite/Devi-Lite/Sketch.cpp
@@ -23,8 +23,13 @@
 #pragma GCC optimize (OPTIMIZATION)
 
 #include <MozziGuts.h> // at the top of your sketch
+#include <Metronome.h> 
+#include <mozzi_rand.h>
+#include <mozzi_fixmath.h>
+#include <IntMap.h>
 #include "DeviHardware.h"
 #include "Globals.h"
+#include "Voice.h"
 
 //Beginning of Auto generated function prototypes by Atmel Studio
 
@@ -46,8 +51,12 @@ void updateRotary();
 * This is the main function that selects which mode to run (sequencer, trigger, edit or save)
 */
 void runMode(); 
-//! Sequencer function, updated in runMode() according to "mode" variable.
-void sequencer();
+//! Update sequencer parameters only in Sequencer Mode.
+void updateSequencer();
+//! Run sequence. It can execute in every Mode as long as "seqOnOff" is true.
+void runSequencer();
+//! Function to increment steps according to step mode
+void incStep();
 //! Trigger function, updated in runMode() according to "mode" variable.
 void trigger();
 //! Edit function, updated in runMode() according to "mode" variable.
@@ -56,34 +65,65 @@ void edit();
 void save();
 //! Load function loads presets using the rotary switch when in sequencer or trigger mode
 void load(int preset);
+//! Turn leds1~6 on 
+void ledsOn(); 
+//! Turn leds1~6 off
+void ledsOff();
 
 
 //End of Auto generated function prototypes by Atmel Studio
 
+// Instantiated objects
+Voice voice[NUMVOICES]; 
+IntMap mapFilterFreq(0, 255, MINFILTER, MAXFILTER); 
 
 void setup() {
 	Serial.begin(9600);
 	startMozzi(CONTROL_RATE); // setup mozzi 
 	setupFastAnalogRead(FASTEST_ADC); // use mozzi analogRead
-
+	
 	// Pin Setup (1=output, 0=input)
-	DDRH = DDRH & B01111011; // Set leds1~6 as outputs
-	DDRB = DDRB & B00010000; // Set Function led as output
-	DDRE = DDRE & B00111000; // Set Mode (RGB) led as output
-	PORTE |= modeColors[mode]; // Light Mode led at startup
-	//sbi(PORTE, 3);
+	//DDRH = DDRH & B01111011; // Set leds1~6 as outputs
+	pinMode(17, OUTPUT);
+	pinMode(16, OUTPUT);
+	pinMode(6, OUTPUT);
+	pinMode(7, OUTPUT);
+	pinMode(8, OUTPUT);
+	pinMode(9, OUTPUT);
+	// Set Function led as output (watch out, port manipulation was messing with Mozzi)
+	pinMode(10, OUTPUT); 
+	// Set Mode (RGB) led as output
+	pinMode(5, OUTPUT);
+	pinMode(3, OUTPUT);
+	digitalWrite(3, HIGH);
+	pinMode(2, OUTPUT);
+	//DDRE = DDRE & B00111000; 
+	//PORTE |= modeColors[mode]; // Light Mode led at startup
 	DDRA = B00000000; // Set all button pins as inputs
 	DDRC = B00000000; // Set rotary switch 1~8 pins as inputs 
-	DDRD = DDRD & B00001111; // Set rotary switch 9~12 pins as inputs (without changing the remaining pins on the same port)
+	//DDRD = DDRD & B00001111; // Set rotary switch 9~12 pins as inputs (without changing the remaining pins on the same port)
+	pinMode(21, INPUT);
+	pinMode(20, INPUT);
+	pinMode(19, INPUT);
+	pinMode(18, INPUT);
+	
+	// Synth Setup
+	for (int i = 0; i < 6; i++)
+	{
+		voice[i].init(CONTROL_RATE, MINPITCH); // init oscillator pitches
+	}
 }
 
 
 void updateControl() {
 	// READ UI HARDWARE
-	// Update Buttons
+	// Read Buttons
 	_buttons = buttons; 
 	buttons = PINA;
-	// Update RotarySwitch 
+	modeLock = !bit_get(buttons, 7); // set Mode lock
+	functionLock = !bit_get(buttons, 6); // set Function lock 
+	
+	// Read RotarySwitch 
 	_rotary = rotary; // set previous values for state change detection
 	for(int i = 0; i < ROTLOWER; i++){
 		if( bit_get(PINC, BIT(i)) ) rotary = i;
@@ -91,13 +131,15 @@ void updateControl() {
 	for(int i = 0; i < ROTUPPER; i++){
 		if( bit_get(PIND, BIT(i)) ) rotary = i+ROTLOWER;
 	}
-	// Update Knobs
+	// Read Knobs
 	for(int i=0; i < NUMVOICES; i++){
-		knobs[i] = mozziAnalogRead(i) >> 3 ; // convert from 0..1023 to 0..127
+		_knobs[i] = knobs[i]; 
+		knobs[i] = mozziAnalogRead(i) >> 2 ; // convert from 0..1023 to 0..255
 	}
 	
-	// Update UI Variable: Mode
-	if (buttons == B01111111 && buttons != _buttons) {
+	if (buttons == B11111111 && _buttons == B01111111) { // if Mode button released
+		ledsOff();
+		modeLock == false; 
 		mode++; // increment mode
 		if(mode>NUMMODES-1) mode = 0; 
 		cbi(PORTE, 3); // clear RGB pins
@@ -106,13 +148,19 @@ void updateControl() {
 		PORTE |= modeColors[mode]; // set RGB color
 	}
 	
-	if(mode < 2 && _rotary != rotary) load(rotary);
+	//TODO: if(mode < 2 && _rotary != rotary) load(rotary); // load preset
 	runMode(); // run main functions 
-	
 }
 
 int updateAudio() {
-	return 0;
+	return ( (long)	voice[0].next() +
+	voice[1].next() +
+	voice[2].next() +
+	voice[3].next() +
+	voice[4].next() +
+	voice[5].next()
+	)		>>4; // 6 voices(98304 to 98303) >>4 (-6144 to 6143)
+	// 14-bit output: -8192 to 8192
 }
 
 void loop() {
@@ -124,15 +172,19 @@ void loop() {
 void runMode(){
 	switch(mode){
 		case 0: 
-			sequencer();
+			updateSequencer();
+			runSequencer();
 			break;
 		case 1:	
+			runSequencer();
 			trigger();
 			break;
 		case 2:
+			runSequencer();
 			edit();
 			break;
 		case 3: 
+			runSequencer();
 			save();
 			break; 
 		default:
@@ -140,17 +192,220 @@ void runMode(){
 	}
 }			
 	
-void sequencer(){}
+//////////////////////////////////////////////////////////////////////////
+// UPDATE SEQUENCER PARAMETERS ///////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////
+void updateSequencer(){
+	
+	// Sequencer Start/Stop
+	if(buttons == B10111111 && buttons != _buttons){
+		seqOnOff = !seqOnOff; 
+		if(seqOnOff) metro.start(); 
+		else metro.stop();
+	}
+	
+	// Run 6-voice iteration if neither the Mode nor the Function button are pressed
+	if(buttons >= B11000000){ 
+		for(int i = 0; i < NUMVOICES; i++){
+			// set mutes
+			if(bit_get(buttons, i) < 1 && bit_get(buttons, i) != bit_get(_buttons, i) ){
+				stepMutes ^= BIT(i); // flip bit(i)
+			}
+			// set pitch
+			if(knobs[i] != _knobs[i]){
+				voice[i].setPitch(knobs[i] >> 2); 			
+			}
+		}
+	}
+	// FUNCTION Combos
+	if( !bit_get(buttons, 6)){ // while FUNCTION button is pressed
+		// knob 1 sets bpm
+		if(knobs[0] != _knobs[0]) {
+			bpm = knobs[0] + MINBPM;
+			metro.setBPM( bpm*subdivisions[subdivision] * 2);
+		}
+		// knob 2 sets subdivion value
+		if(knobs[1] != _knobs[1]) {
+			subdivision = knobs[1] >> 5; // from 0..255 to 0..7
+			metro.setBPM( bpm*subdivisions[subdivision] * 2);
+		}
+		//knob 3 sets scale
+		if(knobs[2] != _knobs[0]) {
+			//TODO: implement scale feature 
+		}
+		// knob 4 shifts all pitches up or down
+		if(knobs[3] != _knobs[3]) {
+			uint8_t pitchInc = knobs[3] - _knobs[3];
+			for(int i = 0; i < NUMVOICES; i++)
+			{
+				pitchInc += voice[i].pitch;
+				voice[i].setPitch(pitchInc);
+			}
+		}
+		// knob 5 shifts all filter frequencies
+		if(knobs[4] != _knobs[4]) {
+			uint8_t freqInc = knobs[4] - _knobs[4];
+			for(int i = 0; i < NUMVOICES; i++){
+				voice[i].filterfreq += mapFilterFreq(freqInc);
+			}
+		}
+		// knob 6 shifts all filter resonances
+		if(knobs[5] != _knobs[5]) {
+			uint8_t resInc = knobs[5] - _knobs[5];
+			for(int i = 0; i < NUMVOICES; i++){
+				voice[i].res += resInc;
+				//TODO: check if res=0 is glitching...
+			}
+		}
+		
+		// Set Step Mutes
+		for(int i = 0; i < NUMVOICES; i++){
+			if(bit_get(buttons, i) != bit_get(_buttons, i)) stepMutes ^= BIT(i);
+		}
+	}
+	
+	// MODE Combos
+	if(!bit_get(buttons, 7)){ // while MODE button is pressed
+		for(int i = 0; i < NUMVOICES; i++){
+			if(bit_get(buttons, i)) mode = i;
+		}	
+	}
+}
+
+//////////////////////////////////////////////////////////////////////////
+// RUN SEQUENCER /////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////
+void runSequencer(){
+	if ( metro.ready() ){
+		PORTB ^= BIT(4); // toggle PINLED7
+		if ( seqTrigger ) // update sequencer
+		{
+			// Led Sequence
+			if(mode < 1){ // Run leds1~6 only on Sequencer Mode
+			   // light up all leds
+			   ledsOn();
+			   // turn off leds if muted
+			   for(int i = 0; i < NUMVOICES; i++){
+				   if(bit_get(stepMutes, i)) cbi(PORTH, ledSequence[i]);
+			   }
+			   // turn off led on current step 
+			   cbi(PORTH, ledSequence[step]);
+			   
+			   //TODO: since PH2 is not connected to any led and stepMutes has sequential bytes from 0 to 5 how to do this with bit manipulation ? 
+			   /*PORTH |= B01111011;// turn all leds on 
+			   uint8_t leds = (stepMutes << 6);
+			   leds = leds >> 6; // get first two bits
+			   uint8_t upperBits = stepMutes >> 2; 
+			   upperBits = upperBits << 3; // get bits 3, 4, 5 and 6 and shitft left one place
+			   leds |= upperBits; 
+			   leds |= ledSequence[step];
+			   PORTH = leds;*/
+			}
+			// Trigger Envelopes 
+			if(!bit_get(stepMutes, step)) voice[step].startDCA();
+			// Increment Step
+			if ( steps > 1 ) incStep();
+		}
+		seqTrigger = !seqTrigger;
+	}
+	// Update Envelopes
+	voice[0].updateEnvelopes();
+	voice[1].updateEnvelopes();
+	voice[2].updateEnvelopes();
+	voice[3].updateEnvelopes();
+	voice[4].updateEnvelopes();
+	voice[5].updateEnvelopes();
+	// ADSR
+	/*voice[0].updateEnv();
+	voice[1].updateEnv();
+	voice[2].updateEnv();
+	voice[3].updateEnv();
+	voice[4].updateEnv();
+	voice[5].updateEnv();*/
+}
+
+void incStep(){
+	switch(stepMode){
+		case 0: // up
+		step++;
+		if ( step > steps-1)   step = 0;
+		break;
+		case 1: // down
+		step--;
+		if ( step < 0)   step = steps-1;
+		break;
+		case 2: // up&down
+		if(stepDir){ // up
+			step++;
+			if ( step > steps-1){
+				step = steps-2;
+				stepDir = 0;
+			}
+		}
+		else { // down
+			step--;
+			if ( step < 0){
+				step = 1;
+				stepDir = 1;
+			}
+		}
+		break;
+		case 3: // palindrome
+		if(stepDir){ // up
+			step++;
+			if ( step > steps-1){
+				step = steps-1;
+				stepDir = 0;
+			}
+		}
+		else { // down
+			step--;
+			if ( step < 0){
+				step = 0;
+				stepDir = 1;
+			}
+		}
+		break;
+		case 4: // random step
+			step = rand(steps);
+		break;
+			case 5: // random step & pitch
+			step = rand(steps);
+			voice[step].setPitch(rand(knobs[step] >> 2));
+		break;
+		default:
+		break;
+	}
+}
+
 
 void trigger(){}
 
+
 void edit(){}
+
 	
 void save(){}
 
+
 void load(int preset){}
 
+void ledsOn(){
+	sbi(PORTH, 0); // PH0
+	sbi(PORTH, 1); // PH1
+	sbi(PORTH, 3); // PH3
+	sbi(PORTH, 4); // PH4
+	sbi(PORTH, 5); // PH5
+	sbi(PORTH, 6); // PH6
+}
 
-
+void ledsOff(){
+	cbi(PORTH, 0); // PH0
+	cbi(PORTH, 1); // PH1
+	cbi(PORTH, 3); // PH3
+	cbi(PORTH, 4); // PH4
+	cbi(PORTH, 5); // PH5
+	cbi(PORTH, 6); // PH6
+}
 
 #pragma GCC pop_options
